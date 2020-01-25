c19c8a92a877113064c55aa17ded10cb44c0a8a5
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2020-01-25 16:26:08 -0300

    insert item menu in bottom bar

--- app/src/main/java/com/example/bottonappbar/MainActivity.kt
+++ app/src/main/java/com/example/bottonappbar/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.bottonappbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setSupportActionBar(bottomAppBar)
+
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottomappbar_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.app_bar_fav -> println("AAAAAAAAA Fav menu item is clicked!")
+            R.id.app_bar_search -> println("AAAAAAA Search menu item is clicked!")
+        }
+
+        return true
     }
 }
